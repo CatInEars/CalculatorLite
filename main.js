b673f3f190c65b3c292e62077ba9acf853cd4f91
unknown <89234857879@mail.ru>
2019-07-12 20:07:27 +0700

    Calculator

---
+++ main.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+
+let doted    = false,
+    operated = false;
+
+$('.num').click(function() {
+    operated = false;
+    let inputValue = $('.input-field').val(),
+        thisNum    = $(this).text();
+
+    $('.input-field').val(`${inputValue}${thisNum}`);
+}); // end click
+
+$('.operation').click(function() {
+    let inputValue = $('.input-field').val(),
+        thisOper   = $(this).text();
+
+    if(!operated) {
+        doted    = false;
+        operated = true;
+
+        $('.input-field').val(`${inputValue}${thisOper}`);
+    }else {
+        inputValue = inputValue.split('');
+        let help   = inputValue.length - 1;
+        inputValue = inputValue.splice(0, help);
+        inputValue = inputValue.join('');
+
+        $('.input-field').val(`${inputValue}${thisOper}`);
+    }
+}); // end click
+
+$('.result').click(function() {
+    try{
+        let result = eval($('.input-field').val());
+        result = +result.toFixed(10);
+        if(result == Infinity) {
+            alert('Error');
+            $('.clear').click();
+            return
+        }
+        $('.input-field').val(result);
+        if(!(isInteger(result))) {
+            doted = true;
+        }
+    }catch {
+        $('.clear').click();
+    }
+}); // end click
+
+$('.clear').click(function() {
+    $('.input-field').val('');
+    doted = false;
+}); // end click
+
+$('.dot').click(function() {
+    if(!doted) {
+        operated = false;
+        doted = true;
+        let inputValue = $('.input-field').val(),
+            dot        = $(this).text();
+
+        $('.input-field').val(`${inputValue}${dot}`);
+    }
+}); // end click
+
+
+function isInteger(num) {
+  return (num ^ 0) === num;
+}
+
+}); // end ready

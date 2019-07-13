ee898d1aef50a595d04be4f48631078b6216dea1
unknown <89234857879@mail.ru>
2019-07-13 20:59:15 +0700

    CalculatorLite Compleate

--- main.js
+++ main.js
@@ -1,43 +1,60 @@
 $(document).ready(function() {
-
-let doted    = false,
-    operated = false,
-    zeroed   = false,
-    minused  = false;
+// Набор вспомогательных переменных, которые помогут
+// мне отслеживать текущее состояние в калькуляторе
+let doted      = false,
+    operated   = false,
+    zeroed     = false,
+    minused    = false;
 
 $('.num').click(function() {
+    if(doted) {
+        dotedOfNum = true;
+    }
+
     let inputValue = $('.input-field').val(),
         thisNum    = $(this).text();
-        minused    = false;
+        minused    = false; // Убираю состояние "минус" для того, что бы его можно было постаивть снова
 
-    if(zeroed) {
+    if(zeroed) { //Если в поле ввода всего 1 число, и это число 0, то заменяю его новым
         zeroed = false;
         $('.input-field').val(`${thisNum}`);
         return
     }
     operated = false;
 
-    $('.input-field').val(`${inputValue}${thisNum}`);
+    $('.input-field').val(`${inputValue}${thisNum}`); // Вставляю новое значение в .input-field
 }); // end click
 
 $('.operation').click(function() {
-    if(minused) return
     let inputValue    = $('.input-field').val(),
         thisOper      = $(this).text(),
-        inputValueArr = inputValue.split('');
+        inputValueArr = inputValue.split(''); // (*)
         zeroed        = false;
         minused       = false;
 
-    if(inputValueArr.length == 0) {
-        return
+    //(*) - Разбиваю строку на маасив для того что бы в дальнейшем отслеживать наличие
+    // или отсутствие операторов в поле
+
+    if(minused) {
+        inputValue = inputValue.split('');
+        let help   = inputValue.length - 1;
+        inputValue = inputValue.splice(0, help);
+        inputValue = inputValue.join('');
+
+        $('.input-field').val(`${inputValue}${thisOper}`);
     }
 
-    if(!operated) {
-        doted    = false;
+    if(inputValueArr.length == 0) {
+        return
+    } // Если в поле ничего не введено, то и оператор соответсвенно не появится (кроме -, т.к он отдельный оператор)
+
+    if(!operated && inputValueArr[inputValueArr.length - 1] != '.') { // Если ни оператора, ни точки нет, то просто вставляю нового оператора
+        doted    = false; // Меняю знаечния на логичные :D
         operated = true;
 
         $('.input-field').val(`${inputValue}${thisOper}`);
     }else {
+        //Если в поле уже есть точка или операция, то меняю последнее знаечние
         inputValue = inputValue.split('');
         let help   = inputValue.length - 1;
         inputValue = inputValue.splice(0, help);
@@ -47,9 +64,25 @@ $('.operation').click(function() {
     }
 }); // end click
 
+/*
+*
+*  Короче, я устал. Коменчю как даун всё) Тут и так всё понятно)))
+*
+*/
+
 $('.minus').click(function() {
     let inputValue = $('.input-field').val();
-    inputValue = inputValue.split('')
+    inputValue = inputValue.split('');
+
+    if(inputValue[inputValue.length - 1] == '.') {
+        let help   = inputValue.length - 1;
+        inputValue = inputValue.splice(0, help);
+        inputValue = inputValue.join('');
+
+        $('.input-field').val(`${inputValue}-`);
+        inputValue = inputValue.split('');
+    }
+
     if(!minused && inputValue[inputValue.length - 1] != '-') {
         inputValue = inputValue.join('');
         $('.input-field').val(`${inputValue}-`);
@@ -79,8 +112,7 @@ $('.zero').click(function() {
 $('.result').click(function() {
     try{
         let result = eval($('.input-field').val());
-        result = +result.toFixed(10);
-        if(result == Infinity) {
+        if(result == Infinity || isNaN(result) || result == -Infinity) {
             alert('Error');
             $('.clear').click();
             return
@@ -101,9 +133,10 @@ $('.result').click(function() {
 
 $('.clear').click(function() {
     $('.input-field').val('');
-    doted  = false;
-    zeroed = false;
-    minused = false;
+    doted    = false;
+    zeroed   = false;
+    minused  = false;
+    operated = false;
 }); // end click
 
 $('.dot').click(function() {
@@ -140,7 +173,7 @@ $('.backspace').click(function() {
         operated = true;
     }else if(inputValue[inputValue.length-1] == '-') {
         minused = true;
-    }else if(inputValue[inputValue.length-1] == '.') {
+    }else if(inputValue[inputValue.length-1] == '.' || inputValue[inputValue.length-2] == '.') {
         doted = true;
     }
 
